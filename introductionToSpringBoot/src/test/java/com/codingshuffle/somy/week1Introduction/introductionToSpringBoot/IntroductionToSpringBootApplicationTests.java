9fd8fb961bbb40cbe74fe3aba8b12a35a59d4650
Somy Garg <lovegarg236@gmail.com>
2025-08-11 13:51:12 +0530

    1st commit

---
+++ introductionToSpringBoot/src/test/java/com/codingshuffle/somy/week1Introduction/introductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuffle.somy.week1Introduction.introductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
